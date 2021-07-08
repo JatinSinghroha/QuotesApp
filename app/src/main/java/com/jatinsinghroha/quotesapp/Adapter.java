bb41c3ea22726523520d330f61717758e545be57
Jatin Singhroha <my895012@gmail.com>
2021-07-08 19:59:34 +0530

    The Base App

---
+++ app/src/main/java/com/jatinsinghroha/quotesapp/Adapter.java
@@ -0,0 +1,53 @@
+package com.jatinsinghroha.quotesapp;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.jatinsinghroha.quotesapp.models.Quote;
+
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class Adapter extends RecyclerView.Adapter<Adapter.viewHolder> {
+
+    List<Quote> quotes;
+
+    public  Adapter(List<Quote> quotes) {
+        this.quotes = quotes;
+    }
+
+    @NonNull
+    @Override
+    public viewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_design,parent,false);
+        return new viewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull viewHolder holder, int position) {
+        holder.quoteAuthor.setText(quotes.get(position).getAuthor());
+        holder.quoteContent.setText(quotes.get(position).getContent());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return quotes.size();
+    }
+
+
+
+    public class viewHolder extends RecyclerView.ViewHolder{
+        TextView quoteAuthor, quoteContent;
+
+        public viewHolder(@NonNull View itemView) {
+            super(itemView);
+            quoteAuthor = itemView.findViewById(R.id.quoteAuthor);
+            quoteContent = itemView.findViewById(R.id.quoteContent);
+        }
+    }
+}
---
+++ app/src/main/java/com/jatinsinghroha/quotesapp/MainActivity.java
@@ -0,0 +1,42 @@
+package com.jatinsinghroha.quotesapp;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
+import com.jatinsinghroha.quotesapp.models.ListOfQuotes;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MainActivity extends AppCompatActivity {
+    RecyclerView recyclerView;
+    private static String JSON_URL = "https://quotable.io/quotes?page=1";
+    Adapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        recyclerView = findViewById(R.id.recyclerView);
+        fetchQuotes();
+    }
+    private void fetchQuotes() {
+        RequestQueue queue = Volley.newRequestQueue(this);
+        JsonObjectRequest jsonArrayRequest = new JsonObjectRequest(Request.Method.GET, JSON_URL, null, response -> {
+            ListOfQuotes listOfQuotes = new Gson().fromJson(response.toString(), ListOfQuotes.class);
+
+            recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+            adapter = new Adapter(listOfQuotes.getQuotes());
+            recyclerView.setAdapter(adapter);
+        }, error -> Log.d("tag", "onErrorResponse: " + error.getMessage()));
+
+        queue.add(jsonArrayRequest);
+
+    }
+}
